1c29a05718cd59f368705904e4825811d4ac855e
Axeltronic <155306226+axeltronic@users.noreply.github.com>
2026-08-04 16:13:55 -0300

    Refactor gamepad connection handling and UI updates

--- gamepad/app.js
+++ gamepad/app.js
@@ -1,45 +1,43 @@
 let gamepadIndex = null;
 
+
 const status = document.getElementById("connection-status");
 const emptyState = document.getElementById("empty-state");
 const testerUI = document.getElementById("tester-ui");
 
-const controllerName = document.getElementById("controller-name");
-const buttons = document.getElementById("buttons");
-const axes = document.getElementById("axes");
+
+const controllerName =
+document.getElementById("controller-name");
+
+const buttons =
+document.getElementById("buttons");
+
+const axes =
+document.getElementById("axes");
 
 
-// Cuando conecta un joystick
-window.addEventListener("gamepadconnected", (event) => {
 
-    gamepadIndex = event.gamepad.index;
-
-    status.className = "status-connected";
-
-    status.innerHTML =
-    "✅ Joystick conectado: " + event.gamepad.id;
+// Detectar conexión
+window.addEventListener("gamepadconnected", (event)=>{
 
 
-    emptyState.classList.add("hidden");
+    connectGamepad(event.gamepad);
 
-    testerUI.classList.remove("hidden");
-
-
-    controllerName.innerHTML =
-    event.gamepad.id;
-
-
-    console.log("Conectado:", event.gamepad);
 
 });
 
 
-// Cuando desconecta
-window.addEventListener("gamepaddisconnected", () => {
+
+// Detectar desconexión
+window.addEventListener("gamepaddisconnected", ()=>{
+
 
     gamepadIndex = null;
 
-    status.className = "status-disconnected";
+
+    status.className =
+    "status-disconnected";
+
 
     status.innerHTML =
     "Ningún joystick conectado";
@@ -53,29 +51,75 @@ window.addEventListener("gamepaddisconnected", () => {
 });
 
 
-// Actualizar datos del joystick
+
+
+// Función conexión
+function connectGamepad(gamepad){
+
+
+    gamepadIndex = gamepad.index;
+
+
+    status.className =
+    "status-connected";
+
+
+    status.innerHTML =
+    "✅ Joystick conectado";
+
+
+    emptyState.classList.add("hidden");
+
+
+    testerUI.classList.remove("hidden");
+
+
+    controllerName.innerHTML =
+    gamepad.id;
+
+
+    console.log("GAMEPAD:", gamepad);
+
+
+}
+
+
+
+
+
+// Actualizar datos
 function update(){
 
+
     if(gamepadIndex !== null){
+
 
         const gamepad =
         navigator.getGamepads()[gamepadIndex];
 
 
+
         if(gamepad){
+
 
             let pressedButtons = [];
 
 
+
             gamepad.buttons.forEach((button,index)=>{
+
 
                 if(button.pressed){
 
+
                     pressedButtons.push(index);
+
 
                 }
 
+
             });
+
 
 
             buttons.innerHTML =
@@ -86,20 +130,28 @@ function update(){
             "Ninguno";
 
 
+
+
+
             axes.innerHTML =
             gamepad.axes
             .map(value => value.toFixed(2))
             .join(" | ");
 
 
+
         }
+
 
     }
 
 
+
     requestAnimationFrame(update);
 
+
 }
+
 
 
 update();
