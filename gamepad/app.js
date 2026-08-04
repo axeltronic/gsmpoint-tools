4b3c0535fb986016ec162991fa9fac0d6d0ff921
Axeltronic <155306226+axeltronic@users.noreply.github.com>
2026-08-04 15:01:36 -0300

    Create app.js

---
+++ gamepad/app.js
@@ -0,0 +1,71 @@
+let gamepadIndex = null;
+
+const status = document.getElementById("connection-status");
+const emptyState = document.getElementById("empty-state");
+const testerUI = document.getElementById("tester-ui");
+
+
+// Cuando conecta un joystick
+window.addEventListener("gamepadconnected", (event) => {
+
+    gamepadIndex = event.gamepad.index;
+
+    status.className = "status-connected";
+
+    status.innerHTML =
+    "✅ Joystick conectado: " + event.gamepad.id;
+
+
+    emptyState.classList.add("hidden");
+
+    testerUI.classList.remove("hidden");
+
+
+    console.log("Conectado:", event.gamepad);
+
+});
+
+
+// Cuando desconecta
+window.addEventListener("gamepaddisconnected", () => {
+
+    gamepadIndex = null;
+
+    status.className = "status-disconnected";
+
+    status.innerHTML =
+    "Ningún joystick conectado";
+
+
+    emptyState.classList.remove("hidden");
+
+    testerUI.classList.add("hidden");
+
+
+});
+
+
+// Loop del joystick
+function update(){
+
+    if(gamepadIndex !== null){
+
+        const gamepad =
+        navigator.getGamepads()[gamepadIndex];
+
+
+        if(gamepad){
+
+            console.log(gamepad);
+
+        }
+
+    }
+
+
+    requestAnimationFrame(update);
+
+}
+
+
+update();
